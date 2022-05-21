5a03082e492d29d534aae90513571f9f6c0850bd
Dakota Clark <dakota@allclarks.com>
2022-05-20 23:17:07 -0600

    Set AGPLv3 license

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿Console.WriteLine($"[UTC {UtcNow}] Starting...");
+﻿// Weather Report. A Slack app to post David Lynch's daily Weather Report videos.
+// Copyright (C) 2022 Dakota Clark
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Console.WriteLine($"[UTC {UtcNow}] Starting...");
 Console.WriteLine($"[UTC {UtcNow}] Loading RSS feed");
 using var reader = XmlReader.Create("https://www.youtube.com/feeds/videos.xml?channel_id=UCDLD_zxiuyh1IMasq9nbjrA");
 var feed = SyndicationFeed.Load(reader);
--- Usings.cs
+++ Usings.cs
@@ -1,3 +1,19 @@
+// Weather Report. A Slack app to post David Lynch's daily Weather Report videos.
+// Copyright (C) 2022 Dakota Clark
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 global using System.ServiceModel.Syndication;
 global using System.Xml;
 

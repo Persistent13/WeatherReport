f152225e8d6cf06acc7eb6005b64e07e1d976a67
Dakota Clark <dakota@allclarks.com>
2022-05-21 12:48:51 -0600

    Cleanup

--- Program.cs
+++ Program.cs
@@ -23,18 +23,16 @@ Console.WriteLine($"[UTC {UtcNow}] RSS feed parsed");
 Console.WriteLine($"[UTC {UtcNow}] Filtering RSS feed for today's post");
 var report = feed.Items.Where(i =>
     i.LastUpdatedTime.UtcDateTime.Date == UtcNow.Date &&
-    i.Title.Text.StartsWith("David Lynch's Weather Report") &&
-    i.Links.Any(l =>
-        !string.IsNullOrEmpty(l.Uri.ToString())
-    )
-// We only ever care to have the latest so [0] it is
+    i.Title.Text.StartsWith("David Lynch's Weather Report")
+    // We only ever care to have the latest so [0] it is
 ).FirstOrDefault()?.Links[0].Uri.ToString();
-Console.WriteLine($"[UTC {UtcNow}] Filtering done, printing results");
 
+Console.WriteLine($"[UTC {UtcNow}] Filtering done, printing results");
 Console.WriteLine(
-    "There is {0} weather report today, {2}.{1}",
+    "[UTC {3}] There is {0} weather report today, {2}.{1}",
     string.IsNullOrEmpty(report) ? "not presently a" : "a",
     string.IsNullOrEmpty(report) ? string.Empty : $" {report}",
-    DateTime.UtcNow.ToString("M/d/yy")
+    UtcNow.ToString("M/d/yy"),
+    UtcNow
 );
 Console.WriteLine($"[UTC {UtcNow}] Stopping...");
